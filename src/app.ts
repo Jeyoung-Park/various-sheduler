7a3a02d1611a6e4d08ac04e0eb38c84e022e2dff
Jeyoung-Park <jason9380@naver.com>
2022-09-27 22:41:25 +0900

    chore: octokit 설치

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
 import express, { Request, Response, NextFunction } from "express";
 import schedule from "node-schedule";
+import { Octokit } from "octokit";
 
-schedule.scheduleJob("0 0 22 * * *", () => {
+schedule.scheduleJob("0 0 22 * * *", async () => {
   // Todo: 잔디 체크 로직을 매일 밤 10시마다 실행
 });
 
 const app = express();
 
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
+app.get("/", async (req: Request, res: Response, next: NextFunction) => {
   res.send("welcome!");
 });
 
