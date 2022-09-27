9d4ab2ef2be93e895b4aafca722b63cb36f17748
Jeyoung-Park <jason9380@naver.com>
2022-09-27 21:48:56 +0900

    feat: node-schedule 적용

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,9 @@
 import express, { Request, Response, NextFunction } from "express";
+import schedule from "node-schedule";
+
+schedule.scheduleJob("0 0 22 * * *", () => {
+  // Todo: 잔디 체크 로직을 매일 밤 10시마다 실행
+});
 
 const app = express();
 
