b9e21adbb7aefee00a2a9d0ce0ad80737dc6dc34
Jeyoung-Park <59302192+Jeyoung-Park@users.noreply.github.com>
2022-10-17 23:54:12 +0900

    feat: user model 정의

---
+++ models/user.ts
@@ -0,0 +1,28 @@
+const Sequelize = require("sequelize");
+
+module.exports = class User extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        name: {
+          type: Sequelize.STRING(20),
+          allowNull: true,
+        },
+        github_id: {
+          type: Sequelize.STRING(30),
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        timestamps: false,
+        underscored: false,
+        modelName: "User",
+        tableName: "users",
+        paranoid: false,
+        charset: "utf8",
+        collate: "utf8_general_ci",
+      }
+    );
+  }
+};
