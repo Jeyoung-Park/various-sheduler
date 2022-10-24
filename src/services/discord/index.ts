b20806d5a3d8a74e7baa1a93d47fcc8e7627fbe2
Jeyoung-Park <jason9380@naver.com>
2022-10-25 00:58:23 +0900

    feat: cau 관련 함수

--- src/services/discord/index.ts
+++ src/services/discord/index.ts
@@ -1,13 +1,21 @@
 const { GatewayIntentBits, Client } = require("discord.js");
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const cauClient = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 export const login = () => {
   client.login(process.env.DISCORD_BOT_TOKEN);
+  cauClient.login(process.env.DISCORD_BOT_TOKEN_CAU);
 };
 
 export const sendDiscordMsg = (msg = "") => {
   if (!process.env.CHANNEL_ID) return;
   const channel = client.channels.cache.get(process.env.CHANNEL_ID);
+  channel?.send(msg);
+};
+
+export const sendDiscordMsgCAU= (msg = "") => {
+  if (!process.env.DISCORD_CHANNEL_ID_UNIV) return;
+  const channel = cauClient.channels.cache.get(process.env.DISCORD_CHANNEL_ID_UNIV);
   channel?.send(msg);
 };
