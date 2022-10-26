9fd1aa925f53613e4a55cad2cc9dc1a3ff299bfa
Jeyoung-Park <jason9380@naver.com>
2022-10-27 00:32:18 +0900

    feat: add cau command

--- src/services/discord/commands/cau.ts
+++ src/services/discord/commands/cau.ts
@@ -1,3 +1,7 @@
+import { getCAUData } from "../../../api/cau";
+
+const DIVIDER_STRING='**************************************\n';
+
 const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
@@ -5,6 +9,15 @@ module.exports = {
     .setName("cau")
     .setDescription("get cau notice data"),
   async execute(interaction: any) {
-    await interaction.reply("cau");
+    const data=await getCAUData('창업');
+    const filteredList=data.data.list.map((item:any)=>({id:item.ORD_NO, title:item.SUBJECT, content:item.SUB_CONTENTS, wroteAt:item.WRITE_DATE}))
+    const initialString:string=''
+    const resultInString=filteredList.reduce((prev:string, cur:any)=>{
+      return prev.concat(DIVIDER_STRING)
+                .concat(`- ${cur.title}\n`)
+                .concat(`- ${cur.content}\n`)
+                .concat(`- ${cur.wroteAt}\n`)
+    }, initialString)
+    await interaction.reply(resultInString);
   },
 };
