79f2dbd2e728c97c8d61c800fc2236a6334b59cb
Jeyoung-Park <jason9380@naver.com>
2022-10-27 00:17:56 +0900

    feat: /cau command 등록

--- src/services/discord/commands/cau.ts
+++ src/services/discord/commands/cau.ts
@@ -2,9 +2,9 @@ const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("ping")
-    .setDescription("Replies with Pong!"),
+    .setName("cau")
+    .setDescription("get cau notice data"),
   async execute(interaction: any) {
-    await interaction.reply("Pong!");
+    await interaction.reply("cau");
   },
 };
--- src/services/discord/index.ts
+++ src/services/discord/index.ts
@@ -4,10 +4,15 @@ const {
   Events,
   Routes,
   REST,
+  Collection
 } = require("discord.js");
+const fs = require('node:fs');
+const path = require('node:path');
+
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 export const cauClient = new Client({ intents: [GatewayIntentBits.Guilds] });
+
 
 export const login = () => {
   client.login(process.env.DISCORD_BOT_TOKEN);
@@ -29,42 +34,73 @@ export const sendDiscordMsgCAU = (msg = "") => {
 };
 
 export const handleCAUData = () => {
-  const commands = [
-    {
-      name: "ping",
-      description: "Replies with Pong!",
-    },
-  ];
+  cauClient.commands = new Collection();
 
-  const rest = new REST({ version: "10" }).setToken(
-    process.env.DISCORD_BOT_TOKEN_CAU
-  );
+  const commands=[]
 
+  const commandsPath = path.join(__dirname, 'commands');
+  const commandFiles = fs.readdirSync(commandsPath).filter((file:any) => file.endsWith('.js')||file.endsWith('.ts'));
+
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    // Set a new item in the Collection with the key as the command name and the value as the exported module
+    if ('data' in command && 'execute' in command) {
+      cauClient.commands.set(command.data.name, command);
+    } else {
+      console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
+  }
+
+  // // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+  for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    commands.push(command.data.toJSON());
+  }
+
+  // Construct and prepare an instance of the REST module
+  const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_BOT_TOKEN_CAU);
+
+  // and deploy your commands!
   (async () => {
     try {
-      console.log("Started refreshing application (/) commands.");
+      console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
-      await rest.put(
-        Routes.applicationCommands(process.env.DISCORD_APPLICATION_ID_CAU),
-        { body: commands }
+      // The put method is used to fully refresh all commands in the guild with the current set
+      const data = await rest.put(
+        Routes.applicationGuildCommands(process.env.DISCORD_APPLICATION_ID_CAU, process.env.DISCORD_GUILD_ID),
+        { body: commands },
       );
 
-      console.log("Successfully reloaded application (/) commands.");
+      console.log(`Successfully reloaded ${data.length} application (/) commands.`);
     } catch (error) {
+      // And of course, make sure you catch and log any errors!
       console.error(error);
     }
   })();
+
 
   cauClient.once(Events.ClientReady, () => {
     console.log("discord cau bot Ready!");
   });
 
   cauClient.on(Events.InteractionCreate, async (interaction: any) => {
+    console.log('interactionCreated')
+    
     if (!interaction.isChatInputCommand()) return;
 
-    if (interaction.commandName === "ping") {
-      console.log("!!!!");
-      await interaction.reply("Pong!");
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) {
+      console.error(`No command matching ${interaction.commandName} was found.`);
+      return;
+    }
+
+    try {
+      await command.execute(interaction);
+    } catch (error) {
+      console.error(error);
+      await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
     }
   });
 };
