7103601cd574b3be21dadc3d8b66ceae540cc83e
Jeyoung-Park <59302192+Jeyoung-Park@users.noreply.github.com>
2022-11-01 23:58:56 +0900

    fix: DEFAULT CHARACTER SET, Engine 추가

--- src/db/create_table.sql
+++ src/db/create_table.sql
@@ -2,4 +2,6 @@ CREATE TABLE Users (
     id INT AUTO_INCREMENT PRIMARY KEY,			-- 유저 id
     name VARCHAR(32) NULL,			-- 이름
     github_id VARCHAR(32) NOT NULL,				-- 깃헙 아이디
+    DEFAULT CHARACTER SET = utf8
+    ENGINE = InnoDB;
 );
