1a00de3751cccbe941d48ceea89bccd28133b24b
박제영 <59302192+Jeyoung-Park@users.noreply.github.com>
2022-11-05 21:23:19 +0900

    Update create_table.sql

--- src/db/create_table.sql
+++ src/db/create_table.sql
@@ -1,7 +1,9 @@
-CREATE TABLE Users (
+CREATE SCHEMA `scheduler` DEFAULT CHARACTER SET utf8;
+
+CREATE TABLE scheduler.users (
     id INT AUTO_INCREMENT PRIMARY KEY,			-- 유저 id
     name VARCHAR(32) NULL,			-- 이름
     github_id VARCHAR(32) NOT NULL,				-- 깃헙 아이디
-    DEFAULT CHARACTER SET = utf8
-    ENGINE = InnoDB;
-);
+)
+DEFAULT CHARACTER SET = utf8
+ENGINE = InnoDB;
