e3a6176ac4c5c416c55d7cba281662b2d9667fa4
Jeyoung-Park <jason9380@naver.com>
2023-01-02 21:46:58 +0900

    fix: 오늘 정보만 가져오도록 수정

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,12 @@
 import express, { NextFunction, Request, Response, Router } from "express";
+import { sendSlackMessage } from "../services/slack";
+import { getCAUListInString } from "../services/slack/cau";
 
 const router: Router = express.Router();
 
 router.get("/", async (req: Request, res: Response, next: NextFunction) => {
+  const cauResult=await getCAUListInString();
+  sendSlackMessage(cauResult);
   return res.send("welcome");
 });
 
--- src/services/slack/cau/index.ts
+++ src/services/slack/cau/index.ts
@@ -1,15 +1,19 @@
 import { getCAUData } from "../../../api/cau";
+const dayjs = require('dayjs')
+
 const DIVIDER_STRING='**************************************\n';
 
+// 오늘 업데이트된 정보만 추출
 export const getCAUListInString=async ()=>{
     const data=await getCAUData('창업');
     const filteredList=data.data.list.map((item:any)=>({id:item.ORD_NO, title:item.SUBJECT, content:item.SUB_CONTENTS, wroteAt:item.WRITE_DATE}))
     const initialString:string=''
     const resultInString=filteredList.reduce((prev:string, cur:any)=>{
+      if(!dayjs().isSame(new Date(cur.wroteAt), 'day')) return prev;
       return prev.concat(DIVIDER_STRING)
                 .concat(`- ${cur.title}\n`)
                 .concat(`- ${cur.content}\n`)
                 .concat(`- ${cur.wroteAt}\n`)
     }, initialString);
-    return `중앙대 창업 정보\n${resultInString}`;
+    return resultInString===''?'오늘 업데이트된 중앙대 창업 정보는 없습니다.':`중앙대 창업 정보\n${resultInString}`;
 }
