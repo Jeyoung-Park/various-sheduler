df7728210199d2754eccbedbb39c2fbec18ec6c7
Jeyoung-Park <jason9380@naver.com>
2023-01-09 13:59:56 +0900

    fix: testcode delete

--- src/routes/cronJob.ts
+++ src/routes/cronJob.ts
@@ -1,12 +1,10 @@
 import express, { NextFunction, Request, Response, Router } from "express";
 import { runCronJob } from "../services/cronJob";
-import { scrapCAUData } from "../services/scrap/univ";
 
 const router: Router = express.Router();
 
 router.get("/", async (req: Request, res: Response, next: NextFunction) => {
-  scrapCAUData();
-  // runCronJob();
+  runCronJob();
   res.send("cron job runned");
 });
 
