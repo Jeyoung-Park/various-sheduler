5070a77b43d44ad831c935100fb00a4e55dc8b51
Jeyoung-Park <jason9380@naver.com>
2023-01-09 13:49:27 +0900

    feat: scrap cau startup function

--- src/routes/cronJob.ts
+++ src/routes/cronJob.ts
@@ -1,10 +1,12 @@
 import express, { NextFunction, Request, Response, Router } from "express";
 import { runCronJob } from "../services/cronJob";
+import { scrapCAUData } from "../services/scrap/univ";
 
 const router: Router = express.Router();
 
 router.get("/", async (req: Request, res: Response, next: NextFunction) => {
-  runCronJob();
+  scrapCAUData();
+  // runCronJob();
   res.send("cron job runned");
 });
 
