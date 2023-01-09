b0c159d839a6cebd6f8d8026906cec02602263f8
Jeyoung-Park <jason9380@naver.com>
2023-01-09 12:24:45 +0900

    fix: CNUData scrap ㅅ 로ㅅ로

--- src/services/scrap/univ.ts
+++ src/services/scrap/univ.ts
@@ -1,17 +1,23 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-const KU_DATA_URL =
+const KU_STARTUP_DATA_URL =
   "https://piportal.korea.ac.kr/front/board/list.do?sep_cd=NOTICE";
+
+const CNU_STARTUP_DATA_URL = "https://connect.cnu.ac.kr/startup/startupnotice";
 
 type KUData = {
   title: string;
   createdAt: string;
 };
 
+type CNUData = {
+  title: string;
+};
+
 export const scrapKUData = async () => {
   try {
-    const html = await axios.get(KU_DATA_URL);
+    const html = await axios.get(KU_STARTUP_DATA_URL);
     let ulList: KUData[] = [];
     const $ = cheerio.load(html.data);
     const bodyList = $(
@@ -24,6 +30,24 @@ export const scrapKUData = async () => {
       const createdAt = $(element).find("td.datecreated").text().slice(0, -4);
       ulList.push({ title, createdAt });
     });
+    return ulList;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const scrapCNUData = async () => {
+  try {
+    const html = await axios.get(CNU_STARTUP_DATA_URL);
+    let ulList: CNUData[] = [];
+    const $ = cheerio.load(html.data);
+    const bodyList = $("table.bbs_table > tbody > tr:not(.notice)");
+    bodyList.each((item: any, element: any) => {
+      const title = $(element).find("td.ellipsis.draggable.subject > a").text();
+      // const createdAt = $(element).find("td.datecreated").text().slice(0, -4);
+      ulList.push({ title });
+    });
+    console.log({ ulList });
     return ulList;
   } catch (error) {
     console.error(error);
