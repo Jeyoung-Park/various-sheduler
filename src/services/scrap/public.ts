49cbc39fa79e9400f1aa516a8b5c8e9db4f8cb26
Jeyoung-Park <jason9380@naver.com>
2023-01-11 01:08:04 +0900

    fix: scrap logic 수정

--- src/services/scrap/public.ts
+++ src/services/scrap/public.ts
@@ -11,29 +11,51 @@ interface PublicData {
   link?: string;
 }
 
+const scrapSinglePageDataAsync = (
+  accData: PublicData[],
+  pageNum: number
+): Promise<PublicData[]> => {
+  console.log("scrapSinglePageDataAsync", pageNum);
+  return new Promise((resolve) =>
+    setTimeout(async () => {
+      // const singlePageData = await scrapSinglePageData(startValue);
+      const html = await axios.get(`${K_STARTUP_URL}?page=${pageNum}`);
+      let ulList: PublicData[] = [];
+      const $ = cheerio.load(html.data);
+      const bodyList = $("ul > li");
+      bodyList.each((item: any, element: any) => {
+        const loadedElement = $(element);
+        const titleElement = loadedElement.find(
+          "div.inner > div.right > div.middle > a > div.tit_wrap > p.tit"
+        );
+        const title = titleElement.text().trim();
+        //   const createdAt = loadedElement.find("td.created").text().trim();
+        ulList.push({
+          title,
+          // createdAt,
+          link: K_STARTUP_ORIGIN_URL,
+        });
+      });
+      resolve([...accData, ...ulList]);
+    }, 3000)
+  );
+};
+
+const scrapTotalData = async (pageNumber: number): Promise<PublicData[]> => {
+  let resultData: PublicData[] = [];
+  for (let i = 1; i <= pageNumber; i++) {
+    resultData = await scrapSinglePageDataAsync(resultData, i);
+    console.log({ resultData_length: resultData.length });
+  }
+  return resultData;
+};
+
 export const scrapKStartupData = async () => {
   try {
     const html = await axios.get(K_STARTUP_URL);
-    let ulList: PublicData[] = [];
     const $ = cheerio.load(html.data);
-    const bodyList = $("ul > li");
     const pageNum = $("div.paginate > a").toArray().length;
-    console.log({ pageNum });
-    bodyList.each((item: any, element: any) => {
-      const loadedElement = $(element);
-      const titleElement = loadedElement.find(
-        "div.inner > div.right > div.middle > a > div.tit_wrap > p.tit"
-      );
-      const title = titleElement.text().trim();
-      //   const createdAt = loadedElement.find("td.created").text().trim();
-      ulList.push({
-        title,
-        // createdAt,
-        link: K_STARTUP_ORIGIN_URL,
-      });
-    });
-    console.log({ ulList });
-    return ulList;
+    const data = await scrapTotalData(pageNum);
   } catch (error) {
     console.error(error);
   }
