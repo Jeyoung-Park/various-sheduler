8ce8ae26a04e44fa5cf00c3f361304267a63024d
Jeyoung-Park <jason9380@naver.com>
2023-01-11 00:38:45 +0900

    fix: kstartup 데이터 받아오도록 수정

--- src/routes/cronJob.ts
+++ src/routes/cronJob.ts
@@ -1,10 +1,12 @@
 import express, { NextFunction, Request, Response, Router } from "express";
 import { runCronJob } from "../services/cronJob";
+import { scrapKStartupData } from "../services/scrap/public";
 
 const router: Router = express.Router();
 
 router.get("/", async (req: Request, res: Response, next: NextFunction) => {
-  runCronJob();
+  // runCronJob();
+  scrapKStartupData();
   res.send("cron job runned");
 });
 
---
+++ src/services/scrap/public.ts
@@ -0,0 +1,38 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const K_STARTUP_ORIGIN_URL =
+  "https://www.k-startup.go.kr/web/contents/bizpbanc-ongoing.do";
+const K_STARTUP_URL =
+  "https://www.k-startup.go.kr/web/module/bizpbanc-ongoing_bizpbanc-inquiry-ajax.do";
+
+interface PublicData {
+  title: string;
+  link?: string;
+}
+
+export const scrapKStartupData = async () => {
+  try {
+    const html = await axios.get(K_STARTUP_URL);
+    let ulList: PublicData[] = [];
+    const $ = cheerio.load(html.data);
+    const bodyList = $("ul > li");
+    bodyList.each((item: any, element: any) => {
+      const loadedElement = $(element);
+      const titleElement = loadedElement.find(
+        "div.inner > div.right > div.middle > a > div.tit_wrap > p.tit"
+      );
+      const title = titleElement.text().trim();
+      //   const createdAt = loadedElement.find("td.created").text().trim();
+      ulList.push({
+        title,
+        // createdAt,
+        link: K_STARTUP_ORIGIN_URL,
+      });
+    });
+    console.log({ ulList });
+    return ulList;
+  } catch (error) {
+    console.error(error);
+  }
+};
