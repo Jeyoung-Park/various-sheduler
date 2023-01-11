02a782482aea2a233a119b9e31efcf872d86d18f
Jeyoung-Park <jason9380@naver.com>
2023-01-12 02:54:59 +0900

    fix: cronjob 주석 해제

--- src/services/cronJob/index.ts
+++ src/services/cronJob/index.ts
@@ -13,36 +13,36 @@ import {
 
 export const runCronJob = async () => {
   // 잔디 체크 로직을 매일 밤 11시 59분마다 실행
-  // try {
-  //   const data = await checkJandi();
-  //   const usersWithNoJandi = data
-  //     .reduce((prev, curr) => {
-  //       if (!curr.isJandi) {
-  //         return prev.concat(`${curr.username}, `);
-  //       }
-  //       return prev;
-  //     }, "")
-  //     .slice(0, -2);
-  //   sendDiscordMsg(`잔디 안 심은 사람: ${usersWithNoJandi}`);
-  // } catch (e: any) {
-  //   console.error(e);
-  //   sendDiscordMsg(
-  //     `에러가 발생했습니다: ${e instanceof Error ? e.message : ""}`
-  //   );
-  // }
+  try {
+    const data = await checkJandi();
+    const usersWithNoJandi = data
+      .reduce((prev, curr) => {
+        if (!curr.isJandi) {
+          return prev.concat(`${curr.username}, `);
+        }
+        return prev;
+      }, "")
+      .slice(0, -2);
+    sendDiscordMsg(`잔디 안 심은 사람: ${usersWithNoJandi}`);
+  } catch (e: any) {
+    console.error(e);
+    sendDiscordMsg(
+      `에러가 발생했습니다: ${e instanceof Error ? e.message : ""}`
+    );
+  }
 
   try {
     // 중대 창업 관련 정보 슬랙에 전송
-    // const cauResult = await getCAUListInString();
-    // sendSlackMessage(cauResult);
+    const cauResult = await getCAUListInString();
+    sendSlackMessage(cauResult);
 
-    // // 고대 창업 관련 정보 슬랙에 전송
-    // const kuResult = await getKUListInString();
-    // sendSlackMessage(kuResult);
+    // 고대 창업 관련 정보 슬랙에 전송
+    const kuResult = await getKUListInString();
+    sendSlackMessage(kuResult);
 
-    // // 충남대 창업 관련 정보 슬랙에 전송
-    // const cnuResult = await getCNUListInString();
-    // sendSlackMessage(cnuResult);
+    // 충남대 창업 관련 정보 슬랙에 전송
+    const cnuResult = await getCNUListInString();
+    sendSlackMessage(cnuResult);
 
     // wevity 창업 관련 정보 슬랙에 전송
     const wevityResult = await getWevityListInString();
