39bb140400926e2f5b3eabe7a4a9c96b9deb922e
Jeyoung-Park <jason9380@naver.com>
2023-05-09 03:11:45 +0900

    fix: 중앙대 창업 정보 디스코드에 보내기

--- src/services/cronJob/index.ts
+++ src/services/cronJob/index.ts
@@ -11,24 +11,29 @@ import {
   getKUListInString,
 } from "../slack/univ";
 
+const CHANNEL_ID_CAU =
+  process.env.NODE_ENV !== "production"
+    ? process.env.CHANNEL_ID_CAU_DEV
+    : process.env.CHANNEL_ID_CAU;
+
 export const runCronJob = async () => {
   // 잔디 체크 로직을 매일 밤 11시 59분마다 실행
   try {
-    if (process.env.NODE_ENV !== "production") return;
-
-    const data = await checkJandi();
-    const usersWithNoJandi = data
-      .reduce((prev, curr) => {
-        if (!curr.isJandi) {
-          return prev.concat(`${curr.username}, `);
-        }
-        return prev;
-      }, "")
-      .slice(0, -2);
-    sendDiscordMsg(
-      `잔디 안 심은 사람: ${usersWithNoJandi}`,
-      process.env.CHANNEL_ID_JANDI
-    );
+    if (process.env.NODE_ENV === "production") {
+      const data = await checkJandi();
+      const usersWithNoJandi = data
+        .reduce((prev, curr) => {
+          if (!curr.isJandi) {
+            return prev.concat(`${curr.username}, `);
+          }
+          return prev;
+        }, "")
+        .slice(0, -2);
+      sendDiscordMsg(
+        `잔디 안 심은 사람: ${usersWithNoJandi}`,
+        process.env.CHANNEL_ID_JANDI
+      );
+    }
   } catch (e: any) {
     console.error(e);
     sendDiscordMsg(
@@ -41,6 +46,7 @@ export const runCronJob = async () => {
     // 중대 창업 관련 정보 슬랙에 전송
     const cauResult = await getCAUListInString();
     sendSlackMessage(cauResult);
+    sendDiscordMsg(cauResult, CHANNEL_ID_CAU);
 
     // 고대 창업 관련 정보 슬랙에 전송
     const kuResult = await getKUListInString();
