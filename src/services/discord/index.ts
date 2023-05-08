db74814a766356be12a47942ac21c8f2fefaddc3
Jeyoung-Park <jason9380@naver.com>
2023-05-09 03:00:13 +0900

    chore: env 값 변경

--- src/services/discord/index.ts
+++ src/services/discord/index.ts
@@ -3,11 +3,13 @@ const { GatewayIntentBits, Client } = require("discord.js");
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 export const login = () => {
-  client.login(process.env.DISCORD_BOT_TOKEN);
+  client.login(process.env.DISCORD_BOT_TOKEN_JANDI);
+  client.login(process.env.DISCORD_BOT_TOKEN_CAU);
 };
 
 export const sendDiscordMsg = (msg = "") => {
-  if (!process.env.CHANNEL_ID || process.env.NODE_ENV !== "production") return;
-  const channel = client.channels.cache.get(process.env.CHANNEL_ID);
+  if (!process.env.CHANNEL_ID_JANDI || process.env.NODE_ENV !== "production")
+    return;
+  const channel = client.channels.cache.get(process.env.CHANNEL_ID_JANDI);
   channel?.send(msg);
 };
