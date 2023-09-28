1ac3edaf9539b74e19a4ca24110ee731b8793353
Jeyoung-Park <jason9380@naver.com>
2023-09-28 19:24:31 +0900

    fix: data.ts gitignore에서 제거

---
+++ data.ts
@@ -0,0 +1,11 @@
+interface User {
+  id: number;
+  name: string;
+  github_id: string;
+}
+
+export const USERS: User[] = [
+  { id: 1, name: "코딩의 신", github_id: "Jeyoung-Park" },
+  { id: 2, name: "유말말", github_id: "Felix-Silas" },
+  { id: 3, name: "안효준틀딱", github_id: "hyojunahn111" },
+];
